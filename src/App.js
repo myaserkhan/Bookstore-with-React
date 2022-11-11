fd326dc89586df4d20b0ff806dab291119984a07
Yasir Khan <khanyaser007@gmail.com>
2022-11-11 17:32:39 +0500

    Create homepage and categories page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Books from './pages/Books';
+import Categories from './pages/Categories';
+
 function App() {
   return (
-    <h1>Hello Bookstore</h1>
+    <Routes>
+      <Route path="/" element={<Books />} />
+      <Route path="/categories" element={<Categories />} />
+    </Routes>
   );
 }
 
